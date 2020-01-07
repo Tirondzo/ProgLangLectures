647eaa38a44260e5593e85b4a73cdb25db21c515
Tirondzo <AlexOkonor@gmail.com>
2020-01-07 22:24:00 +0300

    Add Colored and Minified versions to generator
    Colored use xcode style
    Minified is `pdfnup --nup 4x2 --landscape AllInOne-grayscale.pdf`

--- generate-pdf/generate-pdf.js
+++ generate-pdf/generate-pdf.js
@@ -1,20 +1,29 @@
 var markdownpdf = require("markdown-pdf")
   , fs = require("fs")
   , glob = require("glob")
+  , pdfjam = require("pdfjam")
 
 const LECTURES_PATH = "../Lectures/*.md"
 const OUT_MERGED_MD = "./out/AllInOne.md"
-const OUT_FINAL_PDF = "./out/AllInOne.pdf"
+const OUT_BLACK_PDF = "./out/AllInOne-grayscale.pdf"
+const OUT_COLOR_PDF = "./out/AllInOne-colored.pdf"
+const OUT_NUPED_PDF = "./out/AllInOne-gs-nup.pdf"
+const OUT_FILES = [OUT_MERGED_MD, OUT_BLACK_PDF, OUT_COLOR_PDF, OUT_NUPED_PDF]
 
-const options = {
+const options_black = {
   // Styles are taken from here: https://highlightjs.org/download/
   highlightCssPath:"./highlight_styles/ascetic.css",
   paperBorder:"1cm"
 }
 
+const options_color = Object.assign({}, options_black, {
+  highlightCssPath:"./highlight_styles/xcode.css"
+})
+
 var getDirName = require('path').dirname
-fs.mkdirSync(getDirName(OUT_MERGED_MD), { recursive: true })
-fs.mkdirSync(getDirName(OUT_FINAL_PDF), { recursive: true })
+OUT_FILES.forEach((f)=>{
+  fs.mkdirSync(getDirName(f), { recursive: true })
+})
 
 async function mergeFiles(files, destination)
 {
@@ -28,7 +37,16 @@ var files2print = glob.sync(LECTURES_PATH)
 console.log(files2print)
 
 mergeFiles(files2print, OUT_MERGED_MD).then(()=>{
-  markdownpdf(options).from(OUT_MERGED_MD).to(OUT_FINAL_PDF, function () {
-    console.log("PDF Created")
+  markdownpdf(options_black).from(OUT_MERGED_MD).to(OUT_BLACK_PDF, async function () {
+    console.log("PDF Grayscale Created")
+    pdfjam.nup(OUT_BLACK_PDF, 4, 2,  {
+        orientation: 'landscape',
+        outfile: OUT_NUPED_PDF
+    }).then(() => {
+      console.log("PDF Nuped Created")
+    })
+  })
+  markdownpdf(options_color).from(OUT_MERGED_MD).to(OUT_COLOR_PDF, async function () {
+    console.log("PDF Colored Created")
   })
 })
