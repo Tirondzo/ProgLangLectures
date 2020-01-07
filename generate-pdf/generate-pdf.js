9b553dd27d4d8212a2186d06384ed9171bc0d8fd
Tirondzo <AlexOkonor@gmail.com>
2020-01-07 19:37:39 +0300

    Automatically generate PDF and Merged Markdown
    Merge all lectures to one file.
    Render them to pdf with that package: 
    https://www.npmjs.com/package/markdown-pdf

---
+++ generate-pdf/generate-pdf.js
@@ -0,0 +1,35 @@
+var markdownpdf = require("markdown-pdf")
+  , fs = require("fs")
+  , glob = require("glob")
+
+const LECTURES_PATH = "../Lectures/*.md"
+const OUT_MERGED_MD = "./out/AllInOne.md"
+const OUT_FINAL_PDF = "./out/AllInOne.pdf"
+
+const options = {
+  // Styles are taken from here: https://highlightjs.org/download/
+  highlightCssPath:"./highlight_styles/ascetic.css",
+  paperBorder:"1cm"
+}
+
+var getDirName = require('path').dirname
+fs.mkdirSync(getDirName(OUT_MERGED_MD), { recursive: true })
+fs.mkdirSync(getDirName(OUT_FINAL_PDF), { recursive: true })
+
+function mergeFiles(files, destination)
+{
+  return Promise.all(files.map(async f => {
+    return fs.readFileSync(f)
+  })).then(results => {
+    fs.writeFileSync(destination, results.join("\n"))
+  })
+}
+
+var files2print = glob.sync(LECTURES_PATH)
+console.log(files2print)
+
+mergeFiles(files2print, OUT_MERGED_MD)
+
+markdownpdf(options).from(OUT_MERGED_MD).to(OUT_FINAL_PDF, function () {
+  console.log("PDF Created")
+})
